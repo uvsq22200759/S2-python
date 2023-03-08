cc562ff1c268c1809006937a6b989285e82ea016
Paul <paul.lemonnier@ens.uvsq.fr>
2023-03-08 11:08:56 +0100

    projet interace graphique 1.0

--- README/Sudoku.py
+++ README/Sudoku.py
@@ -1,7 +1,35 @@
-#Progamme Sudoku groupe TD01 S2INTD01
-
 import tkinter as tk
+import random as rd
 
-racine = tk.Tk() # Création de la fenêtre racine
+racine = tk.Tk()
 racine.title("Sudoku")
-racine.mainloop() # Lancement de la boucle principale
+largeur = 350
+hauteur = 350
+largeur_case = largeur // 9
+hauteur_case = hauteur // 9
+epaisseur = 5
+canvas = tk.Canvas(racine, bg="white", height=hauteur, width=largeur)
+canvas.grid()
+
+for i in range(9):
+    for j in range(9):
+        #TEXTE POUR LES CASES PEUT ETRE A INSERER ICI
+        canvas.create_rectangle((i*largeur_case, j*hauteur_case),
+                ((i+1)*largeur_case, (j+1)*hauteur_case), fill="white")
+        
+
+###############CASES EN GRAS MAIS FONCTIONNE PAS########################################################
+#for i in range(10):
+#    if i % 3 == 0:
+#        canvas.create_line(0, i*hauteur_case, hauteur_case, i*hauteur_case, width=epaisseur)
+#    else:
+#        canvas.create_line(0, i*hauteur_case, hauteur_case, i*hauteur_case)
+#            
+#    if i % 3 == 0:
+#        canvas.create_line(i*largeur_case, 0, i*largeur_case, largeur_case, width=epaisseur)
+#    else:
+#            canvas.create_line(0, i*largeur_case, largeur_case, i*largeur_case)
+########################################################################################################
+
+canvas.pack()
+racine.mainloop()
--- README/Untitled-1.py
+++ README/Untitled-1.py
@@ -1,12 +1,37 @@
-# coding: utf-8
- 
-from tkinter import * 
+import tkinter as tk
+import random as rd
 
-fenetre = Tk()
-fenetre.title("Sudoku")
-label = Label(fenetre, text="Sudoku")
-label.pack()
+racine = tk.Tk()
+racine.title("Sudoku")
+largeur = 350
+hauteur = 350
+largeur_case = largeur // 9
+hauteur_case = hauteur // 9
+epaisseur = 5
+canvas = tk.Canvas(racine, bg="white", height=hauteur, width=largeur)
+canvas.grid()
 
-fenetre.mainloop()
+for i in range(9):
+    for j in range(9):
+        #TEXTE POUR LES CASES PEUT ETRE A INSERER ICI
+        canvas.create_rectangle((i*largeur_case, j*hauteur_case),
+                ((i+1)*largeur_case, (j+1)*hauteur_case), fill="white")
+        
+
+###############CASES EN GRAS MAIS FONCTIONNE PAS########################################################
+#for i in range(10):
+#    if i % 3 == 0:
+#        canvas.create_line(0, i*hauteur_case, hauteur_case, i*hauteur_case, width=epaisseur)
+#    else:
+#        canvas.create_line(0, i*hauteur_case, hauteur_case, i*hauteur_case)
+#            
+#    if i % 3 == 0:
+#        canvas.create_line(i*largeur_case, 0, i*largeur_case, largeur_case, width=epaisseur)
+#    else:
+#            canvas.create_line(0, i*largeur_case, largeur_case, i*largeur_case)
+########################################################################################################
+
+canvas.pack()
+racine.mainloop()
 
  
