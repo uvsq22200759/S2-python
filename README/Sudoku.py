241c9f61729188637c6772265e01429560541acb
Paul <paul.lemonnier@ens.uvsq.fr>
2023-03-08 09:19:15 +0100

    projet

---
+++ README/Sudoku.py
@@ -0,0 +1,7 @@
+#Progamme Sudoku groupe TD01 S2INTD01
+
+import tkinter as tk
+
+racine = tk.Tk() # Création de la fenêtre racine
+racine.title("Sudoku")
+racine.mainloop() # Lancement de la boucle principale
