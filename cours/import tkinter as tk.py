fb7a4e26512f95b2cc8c8b0c8b2d60ee10ea0dd7
Paul <paul.lemonnier@ens.uvsq.fr>
2023-03-08 17:51:07 +0100

    avancée

---
+++ cours/import tkinter as tk.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+racine = tk.Tk()
+racine.title("Sudoku")
+ #grille :
+def créer_grille():
+        for i in range(9):
+            for j in range(9):
+                entree = tk.Entry(racine, width=2, font=("Helvetica", 20), justify="center")
+                if j % 3 == 0:
+                    entree.grid(row=i, column=j, padx=(5, 0), pady=(5, 0))
+                else:
+                    entree.grid(row=i, column=j, pady=(5, 0))
+        racine.mainloop()
+créer_grille()
