f31b401c608a3d80c4ccc695e0a9885cb2618666
Paul <paul.lemonnier@ens.uvsq.fr>
2023-03-16 21:39:19 +0100

    dsqdqsd

--- README/Untitled-1.py
+++ README/Untitled-1.py
@@ -1,17 +1,34 @@
 import tkinter as tk
-import random as rd
-
+import random
+from tkinter import filedialog
 racine = tk.Tk()
 racine.title("Sudoku")
- #grille :
+
+# barre de boutons :
+def créer_barre_boutons():
+    barre_boutons = tk.Frame(racine)
+
+    bouton_nouveau = tk.Button(barre_boutons, text="Nouveau", command=créer_grille)
+    bouton_nouveau.pack(side="left", padx=(0, 0))
+
+    bouton_quitter = tk.Button(barre_boutons, text="Quitter", command=racine.destroy)
+    bouton_quitter.pack(side="left", padx=(0, 0))
+
+    barre_boutons.pack(side="top", fill="x", pady=(0, 0))
+
+# grille :
 def créer_grille():
-        for i in range(9):
-            for j in range(9):
-                entree = tk.Entry(racine, width=2, font=("Helvetica", 20), justify="center")  #justify permet de centrer le widget grille
-                if j % 3 == 0:
-                    entree.grid(row=i, column=j, padx=(5, 0), pady=(5, 0))
-                else:
-                    entree.grid(row=i, column=j, pady=(5, 0))
-        racine.mainloop()
+    cadre = tk.Frame(racine)
+    for i in range(9):
+        for j in range(9):
+            entree = tk.Entry(cadre, width=2, font=("Helvetica", 20), justify="center")
+            if j % 3 == 0:
+                entree.grid(row=i, column=j, padx=(5, 0), pady=(5, 0))
+            else:
+                entree.grid(row=i, column=j, pady=(5, 0))
+
+    cadre.pack(side="top", padx=10, pady=(0, 10))
+créer_barre_boutons()
 créer_grille()
+racine.mainloop()
  
